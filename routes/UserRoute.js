94cc2d19ccff4dfbfcbcacfd0136955ba3595e32
danurrizad <danurrizad242@gmail.com>
2023-05-19 15:54:41 +0700

    add error handling on users and add login route

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,18 +1,33 @@
 import express from "express";
+import cors from "cors"
 import { 
     getUsers, 
     getUserById,
     addUser,
     updateUser,
-    deleteUser
+    deleteUser,
+    authLogin
 } from "../controllers/UserController.js";
+import {body} from "express-validator"
 
 const router = express.Router();
 
+//middleware
+router.use(
+    cors({
+        credential: true,
+        origin: 'https://localhost:5173'
+    })
+)
+
 router.get('/users', getUsers);
 router.get('/users/:id', getUserById);
-router.post('/users', addUser);
+router.post('/users',[
+    body('username').isLength({min: 6}).withMessage("Username characters are too short. Minimum character is 5"), 
+    body('password').isLength({min: 8}).withMessage("Password characters are too short. Minimum character is 8")], 
+    addUser);
 router.patch('/users/:id', updateUser);
 router.delete('/users/:id', deleteUser);
+router.post('/login', authLogin);
 
 export default router;
