cf5d053c2518f3348f0c661f9cc7534aa38b947c
danurrizad <danurrizad242@gmail.com>
2023-05-19 15:51:56 +0700

    add env config and add error handling message

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ import mongoose from "mongoose";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 import SkinTriviasRoute from "./routes/SkinTriviasRoute.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/skinfirst-db',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+const db_string = process.env.MONGODB_STRING; 
+// mongoose.connect('mongodb://localhost:27017/skinfirst-db',{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
+mongoose.connect(db_string)
 const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('Database Connected...'))
@@ -18,4 +24,12 @@ app.use(express.json());
 app.use(UserRoute);
 app.use(SkinTriviasRoute);
 
-app.listen(5000, ()=> console.log('Server is up and running...'));
+app.use((error, req, res, next) => {
+    const status = error.errorStatus || 500;
+    const message = error.message;
+    const data = error.data;
+    res.status(status).json({message: message, data: data})
+});
+
+const port = 5000;
+app.listen(port, ()=> console.log(`Server is up and running on port ${port}...`));
